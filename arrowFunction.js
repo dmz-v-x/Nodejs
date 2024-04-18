1c25ba02696a61d31566526876169cc043d82cc9
Himanshu Bhatt <hbhatt034@gmail.com>
2024-04-18 14:09:41 +0530

    Revert "JavaScript Refresher"
    This reverts commit 575854418b7d0103b0615aab477161b10f9feed9.

--- arrowFunction.js
+++
@@ -1,9 +0,0 @@
-const name = "Himanshu";
-let age = 25;
-const hasHobbies = true;
-
-const summarizeUser = (userName, userAge, userHasHobbies) => {
-  return `Name is ${userName} age is ${userAge} and the user has hobbies: ${userHasHobbies}`;
-};
-
-console.log(summarizeUser(name, age, hasHobbies));
